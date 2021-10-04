5d62d033015dad07113d51143963329b447cd4d4
Niko Muukkonen <niko@muukkonen.info>
2021-10-04 17:07:24 +0300

    Create DbContext.

---
+++ OnlineStoreProject/Data/OnlineStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreProject.Data
+{
+    public class OnlineStoreContext : DbContext
+    {
+        public OnlineStoreContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ShopBasket> ShopBaskets { get; set; }
+        public DbSet<ShopBasketRow> ShopBasketRows { get; set; }
+    }
+}
