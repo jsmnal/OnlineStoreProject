606859c94885ec0cf82c59cdc8109f31170d0b49
Niko Muukkonen <niko@muukkonen.info>
2021-10-06 13:15:15 +0300

    Updated Identity table names.

--- OnlineStoreProject/Data/OnlineStoreContext.cs
+++ OnlineStoreProject/Data/OnlineStoreContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OnlineStoreProject.Data.EntityConfigurations;
 using OnlineStoreProject.Models;
@@ -28,6 +29,35 @@ namespace OnlineStoreProject.Data
                 .ApplyConfiguration(new ProductConfiguration())
                 .ApplyConfiguration(new ShopBasketConfiguration())
                 .ApplyConfiguration(new CategoryConfiguration());
+
+            modelBuilder.Entity<OnlineStoreUser>(entity =>
+            {
+                entity.ToTable(name: "User");
+            });
+            modelBuilder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Role");
+            });
+            modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("UserRoles");
+            });
+            modelBuilder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("UserClaims");
+            });
+            modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("UserLogins");
+            });
+            modelBuilder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable("RoleClaims");
+            });
+            modelBuilder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("UserTokens");
+            });
         }
     }
 }
