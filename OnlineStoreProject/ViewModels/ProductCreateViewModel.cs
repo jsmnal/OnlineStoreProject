4f97d2ad3eb012e17d7b3c33ed14b07a2e6f17a3
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-10-10 13:10:23 +0300

    Add ViewModel for creating product

---
+++ OnlineStoreProject/ViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using OnlineStoreProject.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace OnlineStoreProject.ViewModels
+{
+    public class ProductCreateViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IFormFile ImagePath { get; set; }
+        // Product can have one Category
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        // Product can have one Discount
+        public int DiscountId { get; set; }
+        [ForeignKey("DiscountId")]
+        public Discount Discount { get; set; }
+   
+    }
+}
