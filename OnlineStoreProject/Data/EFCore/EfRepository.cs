473633ce7f7ff9c172a2b303d75ea543bdf13d40
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-10-14 23:40:20 +0300

    Add initial IRepository and EfRepository

---
+++ OnlineStoreProject/Data/EFCore/EfRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreProject.Data.EFCore
+{
+    public class EfRepository<T> : IRepository<T> where T : class
+    {
+        private readonly OnlineStoreContext context;
+        public EfRepository(OnlineStoreContext context)
+        {
+            this.context = context;
+        }
+        public async Task<T> Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public Task<T> Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OnlineStoreProject/Data/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreProject.Data
+{
+    public interface IRepository<T> where T: class
+    {
+        Task<T> Get(int Id);
+        Task<T> Delete(int Id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+    }
+}
