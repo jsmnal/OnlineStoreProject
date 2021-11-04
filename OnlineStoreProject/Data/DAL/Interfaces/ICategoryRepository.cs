bd0f7f9905435b490912672794d1aab4e0cc9ed9
Niko Muukkonen <niko@muukkonen.info>
2021-11-04 20:08:30 +0200

    Create ICategoryRepository so we can inject it to controller and its easier to test. Refactor interfaces to own folder.

---
+++ OnlineStoreProject/Data/DAL/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineStoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreProject.Data.DAL
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
