0fcd515b41d0aeae8934d56e249243c51414710f
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-11-13 11:33:48 +0200

    Add Navigation and TitleHeader components

---
+++ Frontend/src/components/TitleHeader.js
@@ -0,0 +1,13 @@
+import { Stack } from 'react-bootstrap';
+
+const TitleHeader = () => {
+    return (
+        <Stack>
+            <div style={{'font-size':'40px'}}>
+                Online Store
+            </div>
+        </Stack>
+    )
+}
+
+export default TitleHeader
