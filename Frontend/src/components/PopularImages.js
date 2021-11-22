7d5469ad83b6c0fbe06b934e5971858e1e4d8c30
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-11-22 20:24:02 +0200

    Update PopularImages component

--- Frontend/src/components/PopularImages.js
+++ Frontend/src/components/PopularImages.js
@@ -4,6 +4,7 @@ const PopularImages = () => {
     return (
     <Container style={{'margin-top':'20px'}}>
         <Row>
+            <h2> Most popular images </h2>
             <Card style={{ 'width': '200px', 'margin':'20px'}}>
                 <Card.Img style={{ 'margin-top': '10px'}} variant="top" src="holder.js/100px180?text=Image here&bg=000000" />
                 <Card.Body>
