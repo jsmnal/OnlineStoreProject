340c31f4796a72ce7412de5855e4772323379b6d
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-12-05 15:53:21 +0200

    Update ImageCard

--- Frontend/src/components/ImageCard.js
+++ Frontend/src/components/ImageCard.js
@@ -1,13 +1,13 @@
 import { Card } from 'react-bootstrap';
 
 
-const ImageCard = (props) => {
+const ImageCard = ({name, price, image}) => {
     return (
     <Card style={{ 'width': '200px', 'margin':'20px', 'boxShadow':'0 0 2px 2px #765d3a'}}>
-        <Card.Img style={{ 'marginTop': '10px'}} variant="top" src={props.image} />
+        <Card.Img style={{ 'marginTop': '10px'}} variant="top" src={image} />
         <Card.Body>
-            <Card.Title>{props.name}</Card.Title>
-            <Card.Title> Price: {props.price}</Card.Title>
+            <Card.Title>{name}</Card.Title>
+            <Card.Title> Price: {price}</Card.Title>
         </Card.Body>
     </Card>
     )
