58623222a55c694039009b683ebe0e3f72ab7015
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-12-14 22:33:32 +0200

    Add Category to routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation';
 import Home from './routes/Home';
 import Product from './routes/Product';
 import NotFound from './routes/404/NotFound';
+import Category from './routes/Category';
 import NotFoundProduct from './routes/404/NotFoundProduct';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route exact path="/404" element={<NotFoundProduct />} />
         <Route exact path="/products" element={<Home />} />
         <Route exact path="/products/:id" element={<Product />} />
+        <Route exact path="/products/category=:category" element={<Category />} />
       </Routes>
     </Router>
   );
