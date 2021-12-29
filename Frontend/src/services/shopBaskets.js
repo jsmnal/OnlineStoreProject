c113fcce73034696a24fb122b9fc1cb9e364be62
Niko Muukkonen <muukkonen.n@gmail.com>
2021-12-29 19:28:52 +0200

    Create shopBaskets service

---
+++ Frontend/src/services/shopBaskets.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import config from '../utils/config';
+
+const URL = `${config.DEV_BACKEND_URL}/api/ShopBaskets/`;
+
+const createShopBasket = async (basket) => {
+  const response = await axios.post(URL, basket, { withCredentials: true });
+  return response.data;
+};
+
+export default { createShopBasket };
