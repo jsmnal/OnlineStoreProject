4dec45cac58e35d466383aed09aa6f9db5e6c1f6
Niko Muukkonen <muukkonen.n@gmail.com>
2022-01-02 20:08:23 +0200

    Update User model and create new migrations

---
+++ OnlineStoreProject/Migrations/20220102180116_UpdateOnlineStoreUserModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineStoreProject.Migrations
+{
+    public partial class UpdateOnlineStoreUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Zip",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Zip",
+                table: "User");
+        }
+    }
+}
