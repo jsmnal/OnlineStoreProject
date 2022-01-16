203ab8bac18b7c9ce9a8edcbefd9d1a490c64770
Jasmin Alian <jasmin.alian@student.careeria.fi>
2022-01-16 09:18:14 +0200

    Add service interfaces

---
+++ OnlineStoreProject/ServiceLayer/Interfaces/IDiscountService.cs
@@ -0,0 +1,18 @@
+﻿using OnlineStoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreProject.ServiceLayer.Interfaces
+{
+    public interface IDiscountService
+    {
+        Task<Discount> Get(int id);
+        Task<Discount> Delete(int id);
+        Task<Discount> Add(Discount discount);
+        Task<Discount> Update(Discount discount);
+        Task<IEnumerable<Discount>> GetAll();
+        Task<Discount> UpdateDiscount(int id, Discount discount);
+    }
+}
---
+++ OnlineStoreProject/ServiceLayer/Interfaces/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using OnlineStoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreProject.ServiceLayer.Interfaces
+{
+    public interface IProductService
+    {
+        Task<Product> Get(int id);
+        Task<Product> Delete(int id);
+        Task<Product> Add(Product product);
+        Task<Product> Update(Product product);
+        Task<IEnumerable<Product>> GetAll();
+        Task<IEnumerable<Product>> GetNewest(int limit);
+        Task<IEnumerable<Product>> GetPopular(int limit);
+        Task<IEnumerable<Product>> GetWithCategory(string name);
+        Task<Product> UpdateProduct(int id, Product product);
+    }
+}
