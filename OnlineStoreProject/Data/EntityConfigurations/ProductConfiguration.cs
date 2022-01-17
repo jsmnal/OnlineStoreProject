5e49d138d653f9539c0417fe9a2c5b4eff4529cb
Niko Muukkonen <muukkonen.n@gmail.com>
2022-01-17 20:18:35 +0200

    Add new products to database when initializing from migrations.
    Also created new migration for this.

--- OnlineStoreProject/Data/EntityConfigurations/ProductConfiguration.cs
+++ OnlineStoreProject/Data/EntityConfigurations/ProductConfiguration.cs
@@ -54,6 +54,114 @@ namespace OnlineStoreProject.Data.EntityConfigurations
                     ImagePath = "winter-forest.jpg",
                     CategoryId = 2,
                     DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 4, 
+                    Name = "Lapland",
+                    Description = "Picture of a beautifull Lapland",
+                    StockQuantity = 27,
+                    Price = 99.99M,
+                    CreatedDate = new DateTime(2021, 12, 01, 09, 30, 00),
+                    ImagePath = "Lappi_1.jpg",
+                    CategoryId = 2,
+                    DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 5, 
+                    Name = "Lapland mountain",
+                    Description = "Mountain of Finland",
+                    StockQuantity = 22,
+                    Price = 19.99M,
+                    CreatedDate = new DateTime(2021, 12, 01, 09, 30, 00),
+                    ImagePath = "Lappi_2.jpg",
+                    CategoryId = 2,
+                    DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 6, 
+                    Name = "Lapland forest",
+                    Description = "Forest of Finland",
+                    StockQuantity = 10,
+                    Price = 9.99M,
+                    CreatedDate = new DateTime(2021, 12, 01, 09, 30, 00),
+                    ImagePath = "Lappi_3.jpg",
+                    CategoryId = 2,
+                    DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 7, 
+                    Name = "Lapland 1",
+                    Description = "Mountain of Finland",
+                    StockQuantity = 22,
+                    Price = 19.99M,
+                    CreatedDate = new DateTime(2021, 12, 01, 09, 30, 00),
+                    ImagePath = "Lappi_4.jpg",
+                    CategoryId = 2,
+                    DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 8, 
+                    Name = "Lapland 2",
+                    Description = "Mountain of Finland",
+                    StockQuantity = 22,
+                    Price = 19.99M,
+                    CreatedDate = new DateTime(2021, 12, 02, 09, 30, 00),
+                    ImagePath = "Lappi_5.jpg",
+                    CategoryId = 2,
+                    DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 9, 
+                    Name = "Lapland 3",
+                    Description = "Mountain of Finland",
+                    StockQuantity = 22,
+                    Price = 19.99M,
+                    CreatedDate = new DateTime(2021, 12, 04, 09, 30, 00),
+                    ImagePath = "Lappi_6.jpg",
+                    CategoryId = 2,
+                    DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 10, 
+                    Name = "Lapland 4",
+                    Description = "Mountain of Finland",
+                    StockQuantity = 22,
+                    Price = 8.89M,
+                    CreatedDate = new DateTime(2021, 11, 02, 09, 30, 00),
+                    ImagePath = "Lappi_7.jpg",
+                    CategoryId = 3,
+                    DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 11, 
+                    Name = "Lapland 5",
+                    Description = "Mountain of Finland",
+                    StockQuantity = 22,
+                    Price = 20.00M,
+                    CreatedDate = new DateTime(2021, 11, 01, 09, 30, 00),
+                    ImagePath = "Lappi_8.jpg",
+                    CategoryId = 3,
+                    DiscountId = 1
+                },
+                new Product 
+                {
+                    Id = 12, 
+                    Name = "Lapland 6",
+                    Description = "Mountain of Finland",
+                    StockQuantity = 22,
+                    Price = 89.99M,
+                    CreatedDate = new DateTime(2021, 09, 02, 09, 30, 00),
+                    ImagePath = "Lappi_9.jpg",
+                    CategoryId = 3,
+                    DiscountId = 1
                 }
                 );
             
